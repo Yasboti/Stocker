d760d9c3db55912daeac440f15e719d5215f42cc
Bartek Borowinski <bborowin@gmail.com>
2014-04-16 00:28:11 -0400

    basic data scaling

--- charts/draw.py
+++ charts/draw.py
@@ -1,8 +1,10 @@
 import csv
 import datetime
+import math
 import requests
 
 
+# retrieve data from google
 def getCsv(symbol, seconds, days):
     url = 'http://www.google.com/finance/getprices'
     query = '?q=%s&i=%s&p=%sd&f=d,o,h,l,c,v' % (symbol.upper(), seconds, days)
@@ -24,4 +26,25 @@ def getCsv(symbol, seconds, days):
                 offset = float(line[0])
             ts = datetime.datetime.fromtimestamp(dts+(seconds*offset))
             result.append([ts, float(line[1]), float(line[5])]) # only timestamp / close / volume
+    return result
+
+
+# adjust values for display
+def prepareData(table):
+    pMax = 0
+    pMin = float(sys.maxint)
+    vMax = 0
+    # collect aggregates
+    for row in table:
+        if vMax < row[2]:
+            vMax = row[2]
+        if pMax < row[1]:
+            pMax = row[1]
+        if pMin > row[1]:
+            pMin = row[1]
+    # scale values
+    offset = pMin / (pMax - pMin)
+    result = []
+    for row in table:
+        result.append([row[0], (row[1] - pMin) / (pMax - pMin), math.log(row[2]) / math.log(vMax)])
     return result
