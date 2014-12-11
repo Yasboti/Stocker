eb6b04a6ee261db35fbbc83fb66fcccd8bd122f1
Bartek Borowinski <bborowin@gmail.com>
2014-12-11 12:50:21 -0500

    quick and dirty db import

--- data/store.py
+++ data/store.py
@@ -1,18 +1,23 @@
 import os
 import datetime
 import csv
+import sqlite3
 
 # get all .csv files in current dir
 dataFiles = []
-for root, dirs, files in os.walk('.'):
-    for fname in files:
-         if fname.endswith('.csv'):
-            dataFiles.append(fname)
+for root, dirs, files in os.walk('csv'):
+   print len(files), 'data files found'
+   for fname in files:
+      if fname.endswith('.csv'):
+         dataFiles.append('csv/'+fname)
+
+connection = sqlite3.connect('historical.sl')
+c = connection.cursor()
 
 for fname in dataFiles:
    with open(fname, 'r') as f:
       reader = csv.reader(f)
-      data =  list(reader)
-      print data[0]
-      exit()
-      print fname, len(data)
+      symbol = fname[:-4][4:]
+      data =  [[symbol] + row for row in list(reader)]
+      r = c.executemany('INSERT INTO eod VALUES (?, ?, ?, ?, ?, ?, ?)', data)
+      print symbol, r
